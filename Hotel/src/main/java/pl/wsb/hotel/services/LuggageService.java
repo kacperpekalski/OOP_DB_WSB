0ec2840021fc75f4affe91a613539918cc609df5
J.Gulcz <108622817+MrGulczu@users.noreply.github.com>
2024-04-20 13:39:50 +0200

    Update LuggageService.java
    Dodano:
    → Uzupełnienie metod abstrakcyjnych w klasie LuggageService

--- Hotel/src/main/java/pl/wsb/hotel/services/LuggageService.java
+++ Hotel/src/main/java/pl/wsb/hotel/services/LuggageService.java
@@ -6,20 +6,36 @@ public class LuggageService extends SpecialService {
     }
 
     @Override
-        public void orderService() {
-            // message that hotel is stored client's luggage
-            System.out.println("Hotel is storing your luggage");
-        }
+    public void orderService() {
+        // message that hotel is stored client's luggage
+        System.out.println("Hotel is storing your luggage");
+    }
 
-        @Override
-        public void checkAvailability() {
-            // message that luggage service is available
-            System.out.println("Luggage service is available");
-        }
+    @Override
+    public void checkAvailability() {
+        // message that luggage service is available
+        System.out.println("Luggage service is available");
+    }
 
-        @Override
-        public void updatePrice(double newPrice) {
-            // message that price has been updated
-            System.out.println("Price has been updated");
-        }
+    @Override
+    public void updatePrice(double newPrice) {
+        // message that price has been updated
+        System.out.println("Price has been updated");
+    }
+    @Override
+    public double checkPrice()
+    {
+        return price;
+    }
+
+    @Override
+    public void updateDescription(String newDescription)
+    {
+        this.description = newDescription;
+    }
+    @Override
+    public String getDescription()
+    {
+        return description;
+    }
 }
